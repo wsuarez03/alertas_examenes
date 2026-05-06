0621f0d149bfbf8e14b705ea1c6e1236017d1017
wsuarez03 <wilderalberto2000@gmail.com>
2026-05-06 10:37:32 -0500

    Update alertas_examenes.py

--- alertas_examenes.py
+++ alertas_examenes.py
@@ -8,7 +8,7 @@ from email.mime.text import MIMEText
 from datetime import datetime
 # ================= CONFIGURACION =================
 EXCEL_URL = "https://valserindustriales-my.sharepoint.com/:x:/p/tecnicodeservicios/IQBHL5qr_uy7TaGi8d3JmyrcActHkB__4ttACgX8ASGC47U?e=OT7mJb&download=1"
-CORREOS_DESTINO = ["tecnicodeservicios@valserindustriales.com"]
+CORREOS_DESTINO = ["tecnicodeservicios@valserindustriales.com",sst@valserindustriales.com]
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 SMTP_USER = os.getenv("SMTP_USER")
