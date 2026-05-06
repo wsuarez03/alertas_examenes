d7e17339d0a8efceb21b2dc691e2c96862b915fe
wsuarez03 <wilderalberto2000@gmail.com>
2026-05-06 11:10:26 -0500

    Update alertas_examenes.py

--- alertas_examenes.py
+++ alertas_examenes.py
@@ -11,8 +11,9 @@ from datetime import datetime
 EXCEL_URL = "https://valserindustriales-my.sharepoint.com/:x:/p/tecnicodeservicios/IQBHL5qr_uy7TaGi8d3JmyrcActHkB__4ttACgX8ASGC47U?e=OT7mJb&download=1"
 
 CORREOS_DESTINO = [
-    "tecnicodeservicios@valserindustriales.com",
-    "sst@valserindustriales.com"
+    "sst@valserindustriales.com",
+    "contabilidad@valserindustriales.com",
+    "tecnicodeservicios@valserindustriales.com"
 ]
 
 SMTP_SERVER = "smtp.gmail.com"
