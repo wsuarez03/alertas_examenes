8b0b595e218c20411971cc811c2a7ded3328e4cf
wsuarez03 <wilderalberto2000@gmail.com>
2026-05-20 12:22:43 -0500

    Update alertas_examenes.py

--- alertas_examenes.py
+++ alertas_examenes.py
@@ -8,7 +8,7 @@ from email.mime.text import MIMEText
 from datetime import datetime
 
 # ================= CONFIGURACION =================
-EXCEL_URL = "https://valserindustriales-my.sharepoint.com/:x:/p/tecnicodeservicios/IQBHL5qr_uy7TaGi8d3JmyrcActHkB__4ttACgX8ASGC47U?e=OT7mJb&download=1"
+EXCEL_URL = "EXCEL_URL = "https://valserindustriales-my.sharepoint.com/:x:/p/sst/IQBrBvFaNIKdS4PM-S-5DxgMAS6dHMmzLp9DMHRM2fTcwJ0?e=9XHSzo&download=1"
 
 CORREOS_DESTINO = [
     "sst@valserindustriales.com",
